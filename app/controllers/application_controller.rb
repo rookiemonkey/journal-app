89f883fc2703fd6ab0a4be2dc92da5c3ba0695b9
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-07 17:59:44 +0800

    refactor error handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
 
   include Exceptions::JournalErrors
+  rescue_from ActiveRecord::RecordNotFound, with: :notfound_error
   rescue_from CreateJournalError, with: :create_journal_error
   rescue_from UpdateJournalError, with: :update_journal_error
 
 
-  protected
+  protected  
+
+  def notfound_error(exception)
+    redirect_to categories_path, alert: "Oh snap! That one doesn't exists"
+  end
 
   def create_journal_error(exception)
     flash.now[:alert] = exception.message
--- app/controllers/concerns/exceptions.rb
+++ app/controllers/concerns/exceptions.rb
@@ -5,6 +5,7 @@ module Exceptions
     # no need to define initialize and super
     # just renaming standard error to something
 
+    class NotFoundJournalError < StandardError; end
     class CreateJournalError < StandardError; end
     class UpdateJournalError < StandardError; end
 
