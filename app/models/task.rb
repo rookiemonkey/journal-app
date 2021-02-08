d9faeeb4eea1e86bcc56355e2528f0c9f5ec9e5c
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-08 10:15:49 +0800

    :hammer: add task model validations/tests for deadline

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
 
   belongs_to :category
+  validate :deadline_not_past
 
   validates :name,
             presence: true,
@@ -12,5 +13,16 @@ class Task < ApplicationRecord
 
   validates :deadline,
             presence: true
+
+
+  private
+
+  # deadline is an instance of ActiveSupport::TimeWithZone
+  # available methods aside from .past? 
+  # https://api.rubyonrails.org/v6.1.0/classes/ActiveSupport/TimeWithZone.html
+  def deadline_not_past
+    return if deadline.nil?
+    errors.add(:deadline, "is past") if deadline.past?
+  end
 
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,7 +4,13 @@ require "pp"
 class TaskTest < ActiveSupport::TestCase
 
   def setup
-    @task = Task.create(name: "Task One")
+    category = Category.create(name: "Category One", 
+                              description: ("a"*50))
+
+    @task = Task.create(name: "Task One", 
+                        description: ("a"*50), 
+                        deadline: "2021-03-30", 
+                        category_id: category.id)
   end
 
   test "1. task should reject empty name" do
@@ -12,18 +18,38 @@ class TaskTest < ActiveSupport::TestCase
     assert_not @task.valid?
   end
 
-  test "2. task should reject name with 5 char and less" do
+  test "2. task should reject name with 5 chars and less" do
     @task.name = '1111'
     assert_not @task.valid?
   end
 
-  test "3. task should reject name with 50 char and more" do
+  test "3. task should reject name with 50 chars and more" do
     @task.name = '1'*51
     assert_not @task.valid?
   end
 
   test "4. task should be complete false upon creation" do
     assert_not @task.completed
+  end
+
+  test "5. task should reject empty description" do
+    @task.description = ''
+    assert_not @task.valid?
+  end
+
+  test "6. task should reject description with 150 chars and more" do
+    @task.description = 'a'*151
+    assert_not @task.valid?
+  end
+
+  test "7. task should reject empty deadline" do
+    @task.deadline = ''
+    assert_not @task.valid?
+  end
+
+  test "8. task should reject past dated deadline" do
+    @task.deadline = '2021-02-07'
+    assert_not @task.valid?
   end
 
 end
