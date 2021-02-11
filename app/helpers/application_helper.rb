22bc40d7f3974c7d9f031bca4d5ce67eba924502
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-11 10:32:39 +0800

    :heavy_check_mark: refactor task#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,9 @@ module ApplicationHelper
     hash
   end
 
+  def task_label(is_completed)
+    return '<span class="has-text-success-dark">Completed</span>' if is_completed
+    return '<span class="has-text-warning-dark">Pending</span>' unless is_completed
+  end
+
 end
