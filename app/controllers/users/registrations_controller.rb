37e5aca523fc03cdea3abb536bff543ad8925ea3
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-23 09:01:57 +0800

    :heavy_plus_sign: add profile edit view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,7 +7,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     build_resource(sign_up_params)
 
     resource.save
-    yield resource if block_given?
 
     if resource.persisted?
       if resource.active_for_authentication?
@@ -24,6 +23,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
       set_minimum_password_length
       respond_with resource
     end
+  end
+
+
+  # override the messages and parameters
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+
+    if resource_updated
+      flash[:notice] = 'Hooray! Successfully updated your account!'
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+
+
+  protected
+
+  def account_update_params
+    # devise_parameter_sanitizer.sanitize(:account_update, keys: [:first_name, :last_name])
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 
 end
